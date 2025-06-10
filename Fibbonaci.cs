f7418db98e4210461df80046f28893b4ae40331e
Nitish Khurana <nitishkhurana@yahoo.com>
2025-06-10 13:31:46 +0530

    Test

--- Fibbonaci.cs
+++ Fibbonaci.cs
@@ -5,6 +5,7 @@ public class Fibbonaci
         int first = 0, second = 1, next;
         Console.WriteLine("Fibonacci Series (Iterative Method):");
         Console.Write(first + " " + second + " ");
+        //Update test
 
         for (int i = 2; i < n; i++)
         {
